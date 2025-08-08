c842eabd6a2f0f7c32e73643c8bb7a4f6ec0df6f
Chiza <lualvarezva@est.utn.ac.cr>
2025-08-08 15:32:48 -0600

    Clase AnimalsHashMap
    Se crean sus metodos abstractos

--- src/Animals/AnimalsHashMap.java
+++ src/Animals/AnimalsHashMap.java
@@ -4,10 +4,51 @@
  */
 package Animals;
 
+import Lists.List;
+import java.util.HashMap;
+import java.util.HashSet;
+
 /**
  *
  * @author Luisk
  */
-public class AnimalsHashMap {
+public class AnimalsHashMap implements List<Animal> {
+    HashMap<String,Animal> map;
     
+    public AnimalsHashMap(){
+        this.map = new HashMap<>();
+    }
+
+    @Override
+    public boolean add(Animal t) {
+        if(map.containsKey(t.getId())) return false;
+        map.put(t.getId(), t);
+        return true;
+    }
+
+    @Override
+    public boolean remove(Animal t) {
+        if(!map.containsKey(t.getId())) return false;
+        map.remove(t.getId());
+        return true;
+    }
+
+    @Override
+    public Animal find(Object id) {
+        String strId = String.valueOf(id);
+        return map.get(strId);
+    }
+
+    @Override
+    public void showAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    public HashSet<String> getSpecies(){
+       HashSet<String> set = new HashSet<>();
+       for (Animal animal:map.values()){
+           set.add(animal.getSpecies());
+       }
+       return set;
+    }
 }
